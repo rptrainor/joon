7fa1c6029fa15fb4a27e5384e1dc03b337147886
Ryan Trainor <rptrainor@gmail.com>
2024-06-10 09:53:22 -0300

    set status bar style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,16 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
-import { MachineProvider } from '@/contexts/MachineContext';
+import { StatusBar } from 'react-native';
 export {
   ErrorBoundary,
 } from 'expo-router';
+
+import { MachineProvider } from '@/contexts/MachineContext';
 
 export const unstable_settings = {
   initialRouteName: '(auth)',
@@ -41,11 +43,14 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   return (
-    <MachineProvider>
+    <ThemeProvider value={DefaultTheme}>
+      <StatusBar barStyle="dark-content" />
+      <MachineProvider>
         <Stack>
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
         </Stack>
-    </MachineProvider>
+      </MachineProvider>
+    </ThemeProvider>
   );
 }

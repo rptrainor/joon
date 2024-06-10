1b5016510e83102d9d5010c9814dfd91a284aa33
Ryan Trainor <rptrainor@gmail.com>
2024-06-10 12:20:23 -0300

    add child screen unit tests

--- app/(auth)/__tests__/add-child.test.tsx
+++ app/(auth)/__tests__/add-child.test.tsx
@@ -1,4 +1,3 @@
-// app/(auth)/__tests__/add-child.test.tsx
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react-native';
 import AddChildScreen from '@/app/(auth)/add-child';
@@ -35,18 +34,17 @@ describe('AddChildScreen Component', () => {
   });
 
   test('renders correctly with initial state values', () => {
-    const { debug } = render(
+    render(
       <MachineContextProvider>
         <AddChildScreen />
       </MachineContextProvider>
     );
-    debug(); // This will print the rendered output to the console
     expect(screen.getByText('Add Child')).toBeTruthy();
     expect(screen.getByPlaceholderText("Enter child's name")).toBeTruthy();
     expect(screen.getByPlaceholderText("Enter child's name").props.value).toBe('Alice');
   });
 
-  test('updates the input field and verifies state change', async () => {
+  test('updates the input field and verifies state change', () => {
     render(
       <MachineContextProvider>
         <AddChildScreen />
@@ -58,7 +56,21 @@ describe('AddChildScreen Component', () => {
     expect(input.props.value).toBe('Bob');
   });
 
-  test('handles the cancel action', async () => {
+  test('handles the add child action', () => {
+    render(
+      <MachineContextProvider>
+        <AddChildScreen />
+      </MachineContextProvider>
+    );
+    const input = screen.getByPlaceholderText("Enter child's name");
+    const button = screen.getByTestId('add-child-button');
+
+    fireEvent.changeText(input, 'Bob');
+    fireEvent.press(button);
+    expect(input.props.value).toBe('Bob');
+  });
+
+  test('handles the cancel action', () => {
     render(
       <MachineContextProvider>
         <AddChildScreen />
